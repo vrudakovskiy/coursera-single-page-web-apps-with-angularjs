162d27cde4aa4d4afe0551ecf5ce0e424a2fd220
Vyacheslav Rudakovskiy <vyacheslav.rudakovskiy@gmail.com>
2018-12-02 15:35:38 +0800

    Module 4 Assesment. Added busy-indicator

---
+++ module-4-assignment/src/menuapp/busy-indicator.component.js
@@ -0,0 +1,29 @@
+(function() {
+
+  angular
+    .module('MenuApp')
+    .component('busyIndicator', {
+      templateUrl: 'src/menuapp/busy-indicator.template.html',
+      controller: BusyIndicatorController
+    });
+
+  BusyIndicatorController.$inject = ['$rootScope'];
+  function BusyIndicatorController($rootScope) {
+    var ctrl = this;
+
+    ctrl.visible = false;
+
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
+      ctrl.visible = true;
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams, options) {
+      ctrl.visible = false;
+    });
+
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, options) {
+      ctrl.visible = false;
+    });
+  }
+
+})();
